e1ebf9d51cd403906af1c4d5bf7fafff3f23a02a
SteCard <stefanocardillo14@gmail.com>
2023-08-28 02:51:04 +0200

    Fillable + seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 10; $i++){
+            $project = new Project();
+            $project->title = $faker->word();
+            $project->description = $faker->paragraph();
+            $project->date_of_creation = $faker->dateTimeThisYear();
+            $project->save();
+        }
     }
 }
